cc9f1f092bb00c6deb51091d63b1739acf1a5f8b
Dave Paola <dpaola2@gmail.com>
2013-04-14 19:39:11 -0700

    ensure model to rake task is an ActiveRecord model

--- lib/devlifecycles/rake_tasks.rb
+++ lib/devlifecycles/rake_tasks.rb
@@ -2,5 +2,16 @@ namespace :devlifecycles do
   desc "Synchronizes models with Devlifecycles"
   task :sync => :environment do
     puts "Using API KEY: #{Devlifecycles.api_key}"
+    model_name = ARGV.last
+    model_class = Object
+    begin
+      model_class = model_name.camelize.constantize
+      if model_class.superclass != ActiveRecord::Base
+        raise "#{model_class.superclass}"
+      end
+      puts "Syncing #{model_class}"
+    rescue
+      puts "#{model_name} is not an instance of ActiveRecord::Base -- cannot sync."
+    end
   end
 end
