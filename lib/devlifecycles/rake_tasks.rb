f096ed082c04b8004249ae9d7f314d340b686408
Dave Paola <dpaola2@gmail.com>
2013-04-14 20:09:16 -0700

    wrapper for sync_with_devlifecycles

--- lib/devlifecycles/rake_tasks.rb
+++ lib/devlifecycles/rake_tasks.rb
@@ -2,16 +2,23 @@ namespace :devlifecycles do
   desc "Synchronizes models with Devlifecycles"
   task :sync => :environment do
     puts "Using API KEY: #{Devlifecycles.api_key}"
-    model_name = ARGV.last
+    model_name = ARGV.last.camelize
     model_class = Object
     begin
-      model_class = model_name.camelize.constantize
+      model_class = model_name.constantize
       if model_class.superclass != ActiveRecord::Base
-        raise "#{model_class.superclass}"
+        raise "#{model_name} is not an instance of ActiveRecord::Base -- cannot sync."
       end
-      puts "Syncing #{model_class}"
-    rescue
-      puts "#{model_name} is not an instance of ActiveRecord::Base -- cannot sync."
+
+      if !model_class.method_defined? :sync_with_devlifecycles
+        raise "#{model_name} cannot be synchronized -- did you forget to add uses_devlifecycles to your model definition?"
+      end
+      puts "Syncing #{model_class} (#{model_class.count} instances)"
+      model_class.all.each do |m|
+        puts m.sync_with_devlifecycles
+      end
+    rescue => e
+      puts e
     end
   end
 end
